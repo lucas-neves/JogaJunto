0e13e08c7e1668deb0b8cc6f1b24730f96635fac
lucasn580 <lucas_neves580@hotmail.com>
2016-10-03 16:39:09 -0300

    Teste commit alteração

--- app/src/main/java/com/jogajunto/MainActivity.java
+++ app/src/main/java/com/jogajunto/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Teste
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
 
