8bee138a82df9b98814c5d0e00f831f432cf8161
lucasn580 <lucas_neves580@hotmail.com>
2016-10-31 18:23:11 -0200

    Update Postagem.java

--- app/src/main/java/com/jogajunto/modelo/Postagem.java
+++ app/src/main/java/com/jogajunto/modelo/Postagem.java
@@ -1,17 +1,116 @@
 package com.jogajunto.modelo;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
 /**
- * Created by aluno on 28/10/2016.
+ * Created by aluno on 07/10/2016.
  */
-public class Postagem {
+public class Quadra {
 
-    private Integer userId;
-    private Integer id;
-    private String title;
-    private String body;
+    @SerializedName("Id_Quadra")
+    private Integer Id_Quadra;
 
-    @Override
-    public String toString() {
-        return userId+" "+id+" "+title;
+    @SerializedName("Id_Dono")
+    private Integer Id_Dono;
+    
+    @SerializedName("Id_End")
+    private Integer Id_End;
+    
+    @SerializedName("CoordenateOne")
+    private String CoordenateOne;
+
+    @SerializedName("CoordenateTwo")
+    private String CoordenateTwo;
+
+    @SerializedName("Opcionais")
+    private String Opcionais;
+
+    @SerializedName("Email")
+    private String Tipo_Quadra;
+
+    public Jogador(){
+
+    }
+
+    public Jogador(String nome, String apelido, String _email, String _senha, String genero){
+        this.Email = _email;
+        this.Senha = _senha;
+        this.Nome = nome;
+        this.Apelido = apelido;
+        this.Genero = genero;
+        HorasJogadas = 0;
+        KmCaminhados = 0;
+        Id = 0;
+    }
+
+    public Jogador(String _email, String _senha){
+        Email = _email;
+        Senha = _senha;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        this.Id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        this.Nome = nome;
+    }
+
+    public String getApelido() {
+        return Apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.Apelido = apelido;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String genero) {
+        this.Genero = genero;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        this.Email = email;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String senha) {
+        this.Senha = senha;
+    }
+
+    public int getHorasJogadas() {
+        return HorasJogadas;
+    }
+
+    public void setHorasJogadas(int horasJogadas) {
+        this.HorasJogadas = horasJogadas;
+    }
+
+    public int getKmCaminhados() {
+        return KmCaminhados;
+    }
+
+    public void setKmCaminhados(int kmCaminhados) {
+        this.KmCaminhados = kmCaminhados;
     }
 }
