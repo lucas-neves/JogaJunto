435b21b35e8fd8ac0c20c31c846d0afa3c61ddd8
lucasn580 <lucas_neves580@hotmail.com>
2016-10-31 18:29:46 -0200

    Create Quadra.java

---
+++ app/src/main/java/com/jogajunto/modelo/Quadra.java
@@ -0,0 +1,17 @@
+package com.jogajunto;
+
+/**
+ * Created by Administrator on 14/09/2016.
+ */
+public class Quadra {
+
+    public int id;
+    public String nome = "Rochdalão";
+    public String descricao = "Osasco Fiao";
+    public int icon = R.drawable.quadra1;
+    public int icon1 = R.drawable.maps;
+    public int icon2 = R.drawable.rating;
+
+
+
+}
